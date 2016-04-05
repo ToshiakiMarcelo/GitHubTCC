6c9387ae49fcf525b4bc0314abc279020d8c4301
andinewba <andilockerz@gmail.com>
2016-04-04 22:36:21 -0300

    Coloquei meus paranaue

--- TCC/Assets/Scripts/Walk.cs
+++ TCC/Assets/Scripts/Walk.cs
@@ -10,16 +10,16 @@ public class Walk : AbstractBehavior {
 		running    = false;
 		bool right = inputState.GetButtonValue(inputButtons[0]);
 		bool left  = inputState.GetButtonValue(inputButtons[1]);
-		bool run   = inputState.GetButtonValue(inputButtons[2]);
+//		bool run   = inputState.GetButtonValue(inputButtons[2]);
 		float teste = inputState.GetButtonHoldTime (inputButtons [0]);
 
 		if (right || left) {
 			float tmpSpeed = speed;
 
-			if (run && runMultiplier > 0) {
-				tmpSpeed *= runMultiplier;
-				running   = true;
-			}
+//			if (run && runMultiplier > 0) {
+//				tmpSpeed *= runMultiplier;
+//				running   = true;
+//			}
 
 			float velX      = tmpSpeed * (float)inputState.direction;
 			body2d.velocity = new Vector2(velX, body2d.velocity.y);
