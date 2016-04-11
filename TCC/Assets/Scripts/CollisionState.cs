f00a8a8d6184143b6e30abc4020428f0b80e26f1
Marcelo Toshiaki <marcelotoshiaki.tm@gmail.com>
2016-04-11 18:23:06 -0300

    Revert "Revert "JumpWall""
    This reverts commit f9ff5e772bfee46c7a52705dccffbf5bcaf0420c.

--- TCC/Assets/Scripts/CollisionState.cs
+++ TCC/Assets/Scripts/CollisionState.cs
@@ -15,7 +15,6 @@ public class CollisionState : MonoBehaviour {
 	public void Awake() {
 		inputState = GetComponent<InputState>();
 	}
-	
 
 	public void FixedUpdate() {
 		Vector2 pos = bottomPosition;
@@ -25,18 +24,17 @@ public class CollisionState : MonoBehaviour {
 
 		standing = Physics2D.OverlapCircle(pos, collisionRadius, collisionLayer);
 
-//		pos    = inputState.direction == Directions.Right ? rightPosition : leftPosition;
-//		pos.x += transform.position.x;
-//		pos.y += transform.position.y;
-//
-//		
-//		onWall = Physics2D.OverlapCircle(pos, collisionRadius, collisionLayer);
+		pos    = inputState.direction == Directions.Right ? rightPosition : leftPosition;
+		pos.x += transform.position.x;
+		pos.y += transform.position.y;
+		
+		onWall = Physics2D.OverlapCircle(pos, collisionRadius, collisionLayer);
 	}
 
 	public void OnDrawGizmos() {
 		Gizmos.color = debugCollisionColor;
 
-		var positions = new Vector2[] {bottomPosition};
+		var positions = new Vector2[] { rightPosition, bottomPosition, leftPosition };
 
 		for (int i = 0; i < positions.Length; i++) {
 			var pos = positions[i];
